fe9cc50bb01a9919b6c14674a5b8c4683dcea6e3
Ebrahim Santana <ebrahimsantana35@gmail.com>
2025-09-18 18:13:39 -0300

    added recyclerViews and coil to image

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,19 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
+    id("androidx.navigation.safeargs.kotlin")
 }
 
 android {
     namespace = "com.practica.exchangecrypto"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.practica.exchangecrypto"
         minSdk = 21
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
@@ -33,13 +36,32 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures{
+        viewBinding = true
+    }
 }
 
 dependencies {
+    val navVersion = "2.7.1"
+    val daggerHilt = "2.48"
+
+    //NavComponent
+    implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
+    implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
+
+    //DaggerHilt
+    implementation("com.google.dagger:hilt-android:$daggerHilt")
+    kapt("com.google.dagger:hilt-compiler:$daggerHilt")
+
+    //Coil
+    implementation("io.coil-kt:coil:2.4.0") // load image
+
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,7 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    //plugin to dagger hilt and about navigation
+    id ("com.google.dagger.hilt.android") version "2.48" apply false
+    id ("androidx.navigation.safeargs.kotlin") version "2.7.1" apply false
 }
