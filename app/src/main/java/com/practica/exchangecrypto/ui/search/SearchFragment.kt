c2cdd8c36176e6a1b7f7805b97567d67361bdfcc
Ebrahim Santana <ebrahimsantana35@gmail.com>
2025-10-23 17:33:05 -0400

    we've started to work with SearchFragment.kt, we've done a simple design and we made a little animation to Editext

--- app/src/main/java/com/practica/exchangecrypto/ui/search/SearchFragment.kt
+++ app/src/main/java/com/practica/exchangecrypto/ui/search/SearchFragment.kt
@@ -5,16 +5,46 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.practica.exchangecrypto.R
+import com.practica.exchangecrypto.databinding.FragmentSearchBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SearchFragment : Fragment() {
 
+    private var _binding: FragmentSearchBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.ivBack.setOnClickListener { requireActivity().onBackPressed() }
+
+        binding.etSearch.apply {
+            translationY = 200f
+            alpha = 0f
+
+            animate()
+                .translationY(0f)
+                .alpha(1f)
+                .setDuration(600)
+                .setStartDelay(200)
+                .start()
+        }
+
+
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
